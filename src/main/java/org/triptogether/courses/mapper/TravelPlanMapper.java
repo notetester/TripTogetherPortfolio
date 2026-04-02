e9908e49b55dd98ae45133a684989ad0a23f0baf
박서진 <sjpark2go@gmail.com>
2026-04-02 18:04:18 +0900

    feat: courses 목록 조회 기능 초기 설정

--- src/main/java/org/triptogether/courses/mapper/TravelPlanMapper.java
+++ src/main/java/org/triptogether/courses/mapper/TravelPlanMapper.java
@@ -1,4 +1,11 @@
 package org.triptogether.courses.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.triptogether.courses.vo.TravelPlanVO;
+
+import java.util.List;
+
+@Mapper
 public interface TravelPlanMapper {
+    List<TravelPlanVO> getTravelList(TravelPlanVO travelPlanVO);
 }
--- src/main/java/org/triptogether/courses/service/TravelPlanService.java
+++ src/main/java/org/triptogether/courses/service/TravelPlanService.java
@@ -1,4 +1,9 @@
 package org.triptogether.courses.service;
 
+import org.triptogether.courses.vo.TravelPlanVO;
+
+import java.util.List;
+
 public interface TravelPlanService {
+    List<TravelPlanVO> getTravelList(TravelPlanVO travelPlanVO);
 }
