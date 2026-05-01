a6cd31faa7777fcb1009dc991261f609ecfe96fe
이정국 <nova4545@hanmail.net>
2026-05-01 09:32:51 +0900

    block

--- src/main/java/org/triptogether/config/JacksonConfig.java
+++ src/main/java/org/triptogether/config/JacksonConfig.java
@@ -1,6 +1,9 @@
 package org.triptogether.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +18,9 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper().findAndRegisterModules();
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
     }
 }
